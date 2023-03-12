fbf9bff358bd5de8ed910f1709c517ac52403606
kevin <kevinosburn.shopifydev@gmail.com>
2023-03-12 08:55:22 -0600

    Update test-jackery

--- assets/cs-product-modal.js
+++
@@ -1,9 +0,0 @@
-console.log("sdsafd")
-var popup_btn = document.querySelector('.product-popup-modal__button');
-var guide_btn = document.querySelector('.guid-title');
-console.log(guide_btn);
-guide_btn.addEventListener('click', function() {
-    console.log('Ouch! Stop poking me!');
-  });
-
-  
